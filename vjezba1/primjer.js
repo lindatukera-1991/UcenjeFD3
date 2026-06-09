7b80c7f484a09c077c195ad739e9fc88c3ffa309
lindatukera-1991 <lindatukera@gmail.com>
2026-06-09 13:25:53 +0200

    vjezba1

---
+++ vjezba1/primjer.js
@@ -0,0 +1,13 @@
+// Ovo je primjer jednostavne JavaScript datoteke
+console.log("JavaScript datoteka je uspješno učitana!");
+
+// Funkcija koja prikazuje pozdravnu poruku
+function pozdraviKorisnika() {
+    alert("Pozdrav! Uspješno ste pokrenuli JavaScript funkciju.");
+}
+
+// Primjer jednostavnog izračuna
+let broj1 = 5;
+let broj2 = 10;
+let rezultat = broj1 + broj2;
+console.log("Rezultat zbroja je: " + rezultat);
